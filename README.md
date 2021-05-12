ea1a1b383cec9340386b15341caedbf80a72b855
miggypinaroc <migzpinaroc@gmail.com>
2021-05-12 15:48:36 +0800

    added seed data

--- README.md
+++ README.md
@@ -3,22 +3,19 @@
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
-Things you may want to cover:
-
 * Ruby version
+```ruby 2.7.1p83```
 
 * System dependencies
 
 * Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Start the server with ```rails s```
+* Database creation and initialization
+For a clean database, run the following script:
+```ruby
+rails db:drop
+rails db:create
+rails db:migrate
+rails db: seed
+```
+Drops the current db, creates a new one and migrates all models, then populates the db with fake data.
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker' # https://github.com/faker-ruby/faker
+
 # Mall admin
 Store.create(:name => "Mall Admin", 
     :email => "mall@admin.com",
@@ -15,3 +17,51 @@ Store.create(:name => "Mall Admin",
     :updated_at => Time.now,
     :role => 1)
 
+# Generate some stores
+# Store 2
+Store.create(:name => "Innisfree", 
+    :email => "innisfree@store.com",
+    :password => "password1",
+    :password_confirmation => "password1",
+    :created_at => Time.now,
+    :updated_at => Time.now,
+    :role => 0)
+
+# Store 3
+Store.create(:name => "Nature Republic", 
+    :email => "nature-republic@store.com",
+    :password => "password2",
+    :password_confirmation => "password2",
+    :created_at => Time.now,
+    :updated_at => Time.now,
+    :role => 0)
+
+# some fake stores with Faker (Store 4-7)
+4.times do Store.create(name: Faker::Restaurant.name, 
+    email: Faker::Internet.email(name: Faker::Restaurant.name, separators: '-', domain: 'store'),
+    :password => "Password123",
+    :password_confirmation => "Password123",
+    :created_at => Time.now,
+    :updated_at => Time.now,
+    :role => 0)
+end
+
+20.times do Customer.create(
+    name: Faker::Name.name,
+    email_address: Faker::Internet.email(name: Faker::Name.name, separators: '-'),
+    mobile_number: Faker::PhoneNumber.cell_phone_in_e164,
+    created_at: Faker::Time.between(from: DateTime.now - 3.hours, to: DateTime.now + 3.hours),
+    updated_at: Time.now,
+    store_id: Faker::Number.between(from: 2, to: 7)
+) 
+end
+
+# lets have someone have multiple entries (aka me) and lets say i am positive (pls no)
+3.times do 
+Customer.create(name: 'Miggy Pinaroc',
+    email_address: 'lmap@email.com',
+    mobile_number: '+639123456789',
+    created_at: Faker::Time.between(from: DateTime.now - 3.hours, to: DateTime.now + 3.hours),
+    updated_at: Time.now,
+    store_id: Faker::Number.between(from: 2, to: 7))
+end
